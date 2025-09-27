2c6b0c57924340749525335f8455e61e17944eb6
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-09-27 14:30:21 +0530

    update our server.js file

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 import express from "express";
-import path from "path";
+import path, { dirname } from "path";
 const app = express();
-
+import { fileURLToPath } from "url";
 const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.static(path.join(__dirname,"public")));
 
-app.listen(prompt,()=>console.log(`server is listening at port number ${PORT}`));
+app.listen(PORT,()=>console.log(`server is listening at port number ${PORT}`));
 
 app.get("/board/:boardId",(req,res)=>{
     res.sendFile(path.join(__dirname,"public",index.html));
