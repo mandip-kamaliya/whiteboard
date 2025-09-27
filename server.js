46e2298264e470a780580b14a2e4022bc51b3750
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-09-27 14:05:06 +0530

    added server.js file

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from "express";
-
+import path from "path";
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname,"public")));
+
 app.listen(prompt,()=>console.log(`server is listening at port number ${PORT}`));
 
-
+app.get("/board/:boardId",(req,res)=>{
+    res.sendFile(path.join(__dirname,"public",index.html));
+})
 
