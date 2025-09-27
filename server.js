8f0e0b8e7b4def7d2c37802acdfadb2100bb8429
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-09-27 13:54:53 +0530

    initialized server.js file

--- server.js
+++ server.js
@@ -2,5 +2,9 @@ import express from "express";
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.listen(prompt,()=>console.log(`server is listening at port number ${PORT}`));
+
+
 
