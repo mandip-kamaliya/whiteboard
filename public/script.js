be077c580b8c96abb9ea2c4e8312e5f0ec1c80f2
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-09-29 16:59:29 +0530

    added resizeCanvas() function

--- public/script.js
+++ public/script.js
@@ -3,3 +3,8 @@ const ctx = canvas.getContext("2d");
 
 let isDrawing = false;
 let startX,startY;
+
+function resizeCanvas(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
